ae2fcfdd647d99272a99b4b770d65014e1773136
Luis <lodiuofl@gmail.com>
2018-09-24 22:08:28 -0400

    forms files

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+import numpy as np
 from django.utils.safestring import mark_safe
-
 
 class SlotProfileDataForm(forms.Form):
     L = forms.IntegerField(min_value=2, max_value=3, label='Number of slot types', initial=3 )
@@ -14,3 +16,86 @@ class SlotProfileDataForm(forms.Form):
     invs = forms.FileField(label= mark_safe("Inventory level of each sku <i class='fa fa-question-circle' aria-hidden='true' title='Upload a csv file with as many rows as skus and as many columns as time-periods. Include at least 100 time-periods for a good analysis.'></i>"),
                            help_text= mark_safe("Download an <a href='/static/files/invs.csv'> example </a> with 100 skus"),
                            widget=forms.FileInput(attrs={'accept': ".csv"}))
+
+
+
+    def clean_L(self):
+        return int(self.cleaned_data.get("L"))
+
+    def clean_nskus(self):
+        return int(self.cleaned_data.get("nskus"))
+
+    def clean_alpha(self):
+        return float(self.cleaned_data.get("alpha")) / 100
+
+    def clean_b(self):
+        return float(self.cleaned_data.get("b"))
+
+    def clean_M(self):
+        return int(self.cleaned_data.get("M"))
+
+    def clean_hs(self):
+        csvfile = self.cleaned_data.get("hs")
+        nskus = int(self.cleaned_data.get("nskus"))
+
+        try:
+            hs = np.genfromtxt(csvfile, delimiter=',')
+        except:
+            raise ValidationError(
+                _('could not be read as an array of numbers'),
+            )
+
+        if len(hs.shape) > 1:
+            raise ValidationError(
+                _('must be a one-dimensional array'),
+            )
+        elif hs.shape[0] != nskus:
+            raise ValidationError(
+                _('There are %(v1) pallet height, but %(v2) skus'),
+                params={'v1': str(hs.shape[0]), 'v2': str(nskus)},
+            )
+
+        if np.min(hs) < 0:
+            raise ValidationError(
+                _('There are negative pallet heights'),
+            )
+
+        if np.isnan(np.sum(hs)):
+            raise ValidationError(
+                _('There are non-numeric characters'),
+            )
+
+        return hs
+
+    def clean_invs(self):
+        csvfile = self.cleaned_data.get("invs")
+        nskus = int(self.cleaned_data.get("nskus"))
+
+        try:
+            invs = np.genfromtxt(csvfile, delimiter=',')
+        except:
+            raise ValidationError(
+                _('could not be read as an 2D array of numbers'),
+            )
+
+        if len(invs.shape) != 2:
+            raise ValidationError(
+                _('must be a 2D array'),
+            )
+        elif invs.shape[0] != nskus:
+            raise ValidationError(
+                _('There are %(v1) rows of inventory levels, but %(v2) skus'),
+                params={'v1': str(invs.shape[0]), 'v2': str(nskus)},
+            )
+
+        if np.min(invs) < 0:
+            raise ValidationError(
+                _('There are negative inventory levels'),
+            )
+
+        if np.isnan(np.sum(invs)):
+            raise ValidationError(
+                _('There are non-numeric characters'),
+            )
+
+        return invs
--- myapp/views.py
+++ myapp/views.py
@@ -1,23 +1,27 @@
-
-from django.shortcuts import render
 import json
+from django.shortcuts import render
 from celery.result import AsyncResult
 from django.http import HttpResponse
 from myapp.forms import SlotProfileDataForm
 from myapp.tasks import create_random_user_accounts
 import numpy as np
 
+def read_params(request, form):
+    alpha = form.cleaned_data['alpha']
+    L = form.cleaned_data['L']
+    b = form.cleaned_data['b']
+    hs = form.cleaned_data['hs'] +  b
+    invs = form.cleaned_data['invs']
+    M = form.cleaned_data['M']
+    return hs, invs, alpha, L, M
+
+
 def generate_random_user(request):
     if request.method == 'POST':
         form = SlotProfileDataForm(request.POST, request.FILES)
         if form.is_valid():
-            alpha = form.cleaned_data.get('alpha')
-            L = form.cleaned_data.get('L')
-            b = form.cleaned_data.get('b')
-            hs = form.cleaned_data.get('hs')
-            invs = form.cleaned_data.get('invs')
-            M = form.cleaned_data.get('M')
-            total_user = int(np.mean(hs))
+            hs, invs, alpha, L, M = read_params(request, form)
+            total_user = int(np.mean(hs))*5
             task = create_random_user_accounts.delay(total_user)
             return HttpResponse(json.dumps({'task_id': task.id}), content_type='application/json')
         else:
