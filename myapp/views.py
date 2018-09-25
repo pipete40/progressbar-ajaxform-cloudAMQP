1311796ae15895d9ce0fd700e9dad4a081ca2eb8
Luis <lodiuofl@gmail.com>
2018-09-24 21:13:21 -0400

    mod form data

--- myapp/views.py
+++ myapp/views.py
@@ -7,21 +7,16 @@ from myapp.forms import SlotProfileDataForm
 from myapp.tasks import create_random_user_accounts
 import numpy as np
 
-def read_params(request, form):
-    alpha = form.cleaned_data['alpha']
-    L = form.cleaned_data['L']
-    b = form.cleaned_data['b']
-    hs = form.cleaned_data['hs'] + b
-    invs = form.cleaned_data['invs']
-    M = form.cleaned_data['M']
-    return hs, invs, alpha, L, M
-
-
 def generate_random_user(request):
     if request.method == 'POST':
         form = SlotProfileDataForm(request.POST)
         if form.is_valid():
-            hs, invs, alpha, L, M = read_params(request, form)
+            alpha = form.cleaned_data.get('alpha')
+            L = form.cleaned_data.get('L')
+            b = form.cleaned_data.get('b')
+            hs = form.cleaned_data.get('hs')
+            invs = form.cleaned_data.get('invs')
+            M = form.cleaned_data.get('M')
             total_user = int(np.mean(hs))
             task = create_random_user_accounts.delay(total_user)
             return HttpResponse(json.dumps({'task_id': task.id}), content_type='application/json')
