e7de84e01a29243d09f55f8455fc92cd55e8a82f
luis <lfcard02@louisville.edu>
2018-09-28 14:45:45 -0400

    minimal form

--- mycelery/celery.py
+++ mycelery/celery.py
@@ -1,3 +1,4 @@
+"""
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
@@ -25,3 +26,4 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+"""
--- mycelery/settings.py
+++ mycelery/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
-    'django_storages',
+    'storages',
 ]
 
 MIDDLEWARE = [
