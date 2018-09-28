1f917e5be7ab94b2e07c647c613c121df3a0d724
luis <lfcard02@louisville.edu>
2018-09-28 14:49:25 -0400

    minimal form

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn mycelery.wsgi --timeout 5 --workers=1
+web: gunicorn mycelery.wsgi --timeout 5
--- mycelery/settings.py
+++ mycelery/settings.py
@@ -47,7 +47,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'django_ajax.middleware.AJAXMiddleware',
 ]
 
 ROOT_URLCONF = 'mycelery.urls'
